23d60f2bcf2c2e47404e4cc9b9a2f161d5c995ca
Gabriel Patrola <almeidagabrielaugusto@gmail.com>
2019-04-08 14:01:23 -0400

    Update cadastro.c

--- cadastro.c
+++ cadastro.c
@@ -1,3 +1,12 @@
+/*************************************************************************
+Nome da Disciplina: Estrutura de Dados
+Nome Professor: Leonair Neves Sousa
+Nome Aluno: Gabriel Augusto Queiroz de Almeida
+Turma: VE1
+RGA: 201611901002
+Data Entrega:
+*************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,378 +16,314 @@
 #include <conio2.h>   //Para trabalhar com gotoxy(), textcolor(), textbackground(),clrscr()
 
 typedef struct funcionario{
-	int cod_func;
-	char nome_func[80];
-	char nasc_func[11];
-	char rg_func[13];
-	char cpf_func[15];
-	char cel_func[15];
-	char email_func[50];
-	char data_admissao[11];
-	struct funcionario *prox;
+    int cod_func;
+    char nome_func[80];
+    char nasc_func[11];
+    char rg_func[13];
+    char cpf_func[15];
+    char cel_func[15];
+    char email_func[50];
+    char data_admissao[11];
+    struct funcionario *prox;
 }funcionarios;
 
 typedef struct locacao{
-	int cod_loc;
-	char data_loc[11];
-	char data_devo[11];
-	float valor_total;
-	float juros;
-	float desc;
-	int cod_cliente;
-	int cod_func;
-	char pagamento[8];
-	int cod_filme;
-	struct locacao *prox;
+    int cod_loc;
+    char data_loc[11];
+    char data_devo[11];
+    float valor_total;
+    float juros;
+    float desc;
+    int cod_cliente;
+    int cod_func;
+    char pagamento[8];
+    int cod_filme;
+    struct locacao *prox;
 }locacoes;
 
 typedef struct devolucao{
-	int cod_loc;
-	char data_loc[11];
-	char data_devo[11];
-	float valor_original;
-	float valor_pago;
-	int dias_atrasados;	
-	struct devolucao *prox;
+    int cod_loc;
+    char data_loc[11];
+    char data_devo[11];
+    float valor_original;
+    float valor_pago;
+    int dias_atrasados; 
+    struct devolucao *prox;
 }devolucoes;
 
 typedef struct cliente{
-	int cod_cliente;
-	char nome_cliente[80];
-	char nasc_cliente[11];
-	char rg_cliente[13];
-	char cpf_cliente[15];
-	char cel_cliente[16];
-	char email_cliente[50];
-	char endereco[80];
-	struct cliente *prox;
+    int cod_cliente;
+    char nome_cliente[80];
+    char nasc_cliente[11];
+    char rg_cliente[13];
+    char cpf_cliente[15];
+    char cel_cliente[16];
+    char email_cliente[50];
+    char endereco[80];
+    struct cliente *prox;
 }clientes;
 
 typedef struct genero{
-	int cod_gen;
-	char nome[80];
-	struct genero *prox;
+    int cod_gen;
+    char nome_genero[80];
+    struct genero *prox;
 }generos;
 
 typedef struct filme{
-	int cod_filme;
-	int cod_gen;
-	char nome_filme[80];
-	char ano[5];
-	char classificacao[50];
-	float valor;
-	struct filme *prox;
+    int cod_filme;
+    int cod_gen;
+    char nome_filme[80];
+    char ano[5];
+    char classificacao[50];
+    float valor;
+    struct filme *prox;
 }filmes;
-
-typedef struct Node{
- int num;
- struct Node *prox;
-}node; 
 
 int tam;
 
-void inicia(node *LISTA){
- 	LISTA->prox = NULL;
- 	tam=0;
+void inicia(generos *gen){
+    gen->prox = NULL;
+    tam=0;
 }
 
-int vazia(node *LISTA){
- 	if(LISTA->prox == NULL)
- 	 	return 1;
- 	else
-  		return 0;
+void inicia_filme(filmes *fil){
+    fil->prox = NULL;
+    tam=0;
+}
+void inicia_cliente(clientes *clin){
+    clin->prox = NULL;
+    tam=0;
+}
+void inicia_funcionario(funcionarios *func){
+    func->prox = NULL;
+    tam=0;
+}
+void inicia_locacao(locacoes *loc){
+    loc->prox = NULL;
+    tam=0;
 }
 
-node *aloca(){
- 	node *novo=(node *) malloc(sizeof(node));
- 	if(!novo){
-  		printf("Sem memoria disponivel!\n");
-  		exit(1);
- 	}
-	 else{
-  		printf("Novo elemento: "); scanf("%d", &novo->num);
-  		return novo;
- 	}
+int vazia(generos *gen){
+    if(gen->prox == NULL)
+        return 1;
+        else
+        return 0;
 }
 
-void insereFim(node *LISTA){
-	node *novo=aloca();
- 	novo->prox = NULL;
-	if(vazia(LISTA))
- 		LISTA->prox=novo;
- 	else{
-  	node *tmp = LISTA->prox;
-  		while(tmp->prox != NULL)
-   			tmp = tmp->prox;
-  			tmp->prox = novo;
- }
- 	tam++;
+generos *aloca(){
+    generos *novo=(generos *) malloc(sizeof(generos));
+    if(!novo){
+        printf("Sem memoria disponivel!\n");
+        exit(1);
+    }
+    else{
+        printf("Novo elemento: "); scanf("%d", &novo->cod_gen);
+        return novo;
+    }
 }
 
-void insereInicio(node *LISTA){
- 	node *novo=aloca(); 
- 	node *oldHead = LISTA->prox;
- 	LISTA->prox = novo;
- 	novo->prox = oldHead;
- 	tam++;
+void insereInicio(generos *gen){
+    generos *novo=aloca(); 
+    generos *oldHead = gen->prox;
+    gen->prox = novo;
+    novo->prox = oldHead;
+    tam++;
 }
 
-void exibe(node *LISTA){
- 	system("cls");
- 	if(vazia(LISTA)){
-  		printf("Lista vazia!\n\n");
-  		return ;
- 	}
- 	node *tmp;
- 	tmp = LISTA->prox;
- 	printf("Lista:");
- 	while( tmp != NULL){
-  		printf("%5d", tmp->num);
-  		tmp = tmp->prox;
- 	}
- 	printf("\n        ");
- 	int count;
- 	for(count=0 ; count < tam ; count++){
- 	 	printf("  ^  ");
- 		printf("\nOrdem:");
- 		for(count=0 ; count < tam ; count++){
- 			printf("%5d", count+1);
- 		}
- 	}
- 	printf("\n\n");
+void exibe(generos *gen){
+    system("cls");
+    if(vazia(gen)){
+        printf("gen vazia!\n\n");
+        return;
+    }
+    generos *tmp;
+    tmp = gen->prox;
+    printf("gen:");
+    while( tmp != NULL){
+        printf("%5d", tmp->cod_gen);
+        tmp = tmp->prox;
+    }
+    printf("\n        ");
+    int count;
+    for(count=0 ; count < tam ; count++){
+        printf("  ^  ");
+    }
+        printf("\nOrdem:");
+    for(count=0 ; count < tam ; count++){
+        printf("%5d", count+1);
+    }
+        printf("\n\n");
 }
 
-void libera(node *LISTA){
- 	if(!vazia(LISTA)){
-  		node *proxNode,
-     	*atual;
-  		atual = LISTA->prox;
-  		while(atual != NULL){
-   			proxNode = atual->prox;
-   			free(atual);
-  		 	atual = proxNode;
-  		}
-	 }
+void libera(generos *gen){
+    if(!vazia(gen)){
+        generos *proxgeneros,
+        *atual;
+        atual = gen->prox;
+        while(atual != NULL){
+            proxgeneros = atual->prox;
+            free(atual);
+            atual = proxgeneros;
+        }
+    }
 }
 
-void insere(node *LISTA){
- 	int pos,
-  	count;
- 	printf("Em que posição, [de 1 ate %d] você deseja inserir: ", tam);
- 	scanf("%d", &pos);
- 	if(pos>0 && pos <= tam){
-  		if(pos==1)
-   			insereInicio(LISTA);
-  		else{
-   			node *atual = LISTA->prox,
-     		*anterior=LISTA; 
-   			node *novo=aloca();
-    	 	for(count=1 ; count < pos ; count++){
-     			anterior=atual;
-     			atual=atual->prox;
-   			}
-   			anterior->prox=novo;
-   			novo->prox = atual;
-   			tam++;
-  		}
-   
- 	}
-	else{
-  		printf("Elemento inválido\n\n"); 
-  	}	
+void insere(generos *gen){
+    int pos,
+    count;
+    printf("Em que posicao, [de 1 ate %d] voce deseja inserir: ", tam);
+    scanf("%d", &pos);
+     
+    if(pos>0 && pos <= tam){
+        if(pos==1)
+            insereInicio(gen);
+        else{
+            generos *atual = gen->prox,
+            *anterior=gen; 
+            generos *novo=aloca();
+            for(count=1 ; count < pos ; count++){
+                anterior=atual;
+                atual=atual->prox;
+            }
+            anterior->prox=novo;
+            novo->prox = atual;
+            tam++;
+        }  
+     }
+    else
+        printf("Elemento invalido\n\n");  
 }
 
-node *retiraInicio(node *LISTA){
- 	if(LISTA->prox == NULL){
-  		printf("Lista já está vazia\n");
-  		return NULL;
- 	}
-	 else{
-  		node *tmp = LISTA->prox;
-  		LISTA->prox = tmp->prox;
-  		tam--;
-  		return tmp;
- 	}  
+generos *retiraInicio(generos *gen){
+    if(gen->prox == NULL){
+        printf("gen ja esta vazia\n");
+        return NULL;
+    }
+    else{
+        generos *tmp = gen->prox;
+        gen->prox = tmp->prox;
+        tam--;
+        return tmp;
+    }    
 }
 
-node *retiraFim(node *LISTA){
- 	if(LISTA->prox == NULL){
-  		printf("Lista já vazia\n\n");
-  		return NULL;
- 	}
-	else{
-  		node *ultimo = LISTA->prox,
-    	*penultimo = LISTA;
-  		while(ultimo->prox != NULL){
-   			penultimo = ultimo;
-   			ultimo = ultimo->prox;
-  		}
-   		penultimo->prox = NULL;
-  		tam--;
-  		return ultimo;  
- 	}
-}
-
-node *retira(node *LISTA){
- 	int opt, count;
- 	printf("Que posição, [de 1 ate %d] você deseja retirar: ", tam);
- 	scanf("%d", &opt);
- 	if(opt>0 && opt <= tam){
-  		if(opt==1)
-   			return retiraInicio(LISTA);
-  		else{
-   			node *atual = LISTA->prox,
-     		*anterior=LISTA; 
-   			for(count=1 ; count < opt ; count++){
-    			anterior=atual;
-    			atual=atual->prox;
-   			}
-  			anterior->prox=atual->prox;
-  			tam--;
-  			return atual;
-  		}
- 	}
-	else{
-  		printf("Elemento inválido\n\n");
- 	 	return NULL;
- 	}
+generos *retira(generos *gen){
+    int opt,
+    count;
+    printf("Que posicao, [de 1 ate %d] voce deseja retirar: ", tam);
+    scanf("%d", &opt);
+     
+    if(opt>0 && opt <= tam){
+        if(opt==1)
+            return retiraInicio(gen);
+        else{
+            generos *atual = gen->prox,
+            *anterior=gen; 
+            for(count=1 ; count < opt ; count++){
+                anterior=atual;
+                atual=atual->prox;
+            } 
+            anterior->prox=atual->prox;
+            tam--;
+            return atual;
+        }
+     }
+    else{
+        printf("Elemento invalido\n\n");
+        return NULL;
+    }
 }
 
 int menu(){
-	system("cls");
-	system("Color 37");	
- 	int opt;
- 	 for(int i=24;i<65;i++){
-		gotoxy(i,23);printf("-"); //For para o prenchimento da parte inferior do menu
-	}
-	for(int x = 6; x<23; x++){
-		gotoxy(64, x);printf("|");
-	}	
- 	gotoxy(24,5);printf("------------- MENU PRINCIPAL ------------");
-    gotoxy(24,6);printf("| Digite a sua escolha: \n" );
-    gotoxy(24,7);printf("| 1 - Cadastro Gênero \n" );
-    gotoxy(24,8);printf("| 2 - Cadastro Filmes \n" );
-    gotoxy(24,9);printf("| 3 - Cadastro Cliente \n");
-    gotoxy(24,10);printf("| 4 - Cadastro Funcionário \n" );
-    gotoxy(24,11);printf("| 5 - Cadastro Locação \n" );
-    gotoxy(24,12);printf("| 6 - Fazer Devolução \n");
-    gotoxy(24,13);printf("| 7 - Excluir gênero \n" );
-    gotoxy(24,14);printf("| 8 - Excluir Filmes \n" );
-    gotoxy(24,15);printf("| 9 - Excluir Cliente \n");
-    gotoxy(24,16);printf("| 10 - Excluir Funcionário \n" );
-    gotoxy(24,17);printf("| 11 - Excluir Locação \n" );
-    gotoxy(24,18);printf("| 12 - Listar locação data específica \n");
-    gotoxy(24,19);printf("| 13 - Listar filmes em atraso \n");
-    gotoxy(24,20);printf("| 14 - Listar devolução \n");
-    gotoxy(24,21);printf("| 15 - Sair \n" );
-    gotoxy(24,22);printf("| Opção : " );
+    int opt;
+     
+     printf("Escolha a opcao\n");
+     printf("0. Sair\n");
+     printf("1. Zerar gen\n");
+     printf("2. Exibir gen\n");
+     printf("3. Adicionar generos no inicio\n");
+     printf("4. Adicionar generos no final\n");
+     printf("5. Escolher onde inserir\n");
+     printf("6. Retirar do inicio\n");
+     printf("7. Retirar do fim\n");
+     printf("8. Escolher de onde tirar\n");
+     printf("Opcao: "); scanf("%d", &opt);
+     
+     return opt;
+    }
+
+void opcao(generos *gen, filmes *fil, clientes *clin, funcionarios *func, locacoes *loc, int op){
+    generos *tmp;
+    
+    switch(op){
+    case 0:
+        libera(gen);
+    break;
+       
+    case 1:
+        libera(gen);
+        inicia(gen);
+    break;
+          
+    case 2:
+        exibe(gen);
+    break;
+          
+    case 3:
+        insereInicio(gen);
+    break;  
+           
+    case 4:
+
+    break;
+           
+    case 5:
+        insere(gen);
+    break;
+          
+    case 6:
+        tmp= retiraInicio(gen);
+        printf("Retirado: %3d\n\n", tmp->cod_gen);
+    break;
+           
+    case 7:
+
+    break;
+          
+    case 8:
+        tmp= retira(gen);
+        printf("Retirado: %3d\n\n", tmp->cod_gen);
+    break;
+          
+    default:
+        printf("Comando invalido\n\n");
+    }
 }
 
-
 int main(){
-	setlocale(LC_ALL,"");
-	int opcao, numero;
- 	node *genero = (node *) malloc(sizeof(node));
- 	node *tmp;
- do{
-    menu(); 
-    scanf("%i", &opcao);
-	system("cls");
-        switch (opcao){ 
-            case 0:
-   		libera(genero);
-   		system("pause");
-   	break;
-   
-  	case 1:
-   		libera(genero);
-   		inicia(genero);
-   		system("pause");
-   	break;
-  
-  	case 2:
-   		exibe(genero);
-   		system("pause");
-   	break;
-  
-  	case 3:
-   		insereInicio(genero);
-   		system("pause");
-   	break;  
-   
-  	case 4:
-   		insereFim(genero);
-   		system("pause");
-   	break;
-   
-  	case 5:
-   		insere(genero);
-   		system("pause");
-   	break;
-  
-  	case 6:
-   		tmp= retiraInicio(genero);
-   		printf("Retirado: %3d\n\n", tmp->num);
-   		system("pause");
-   	break;
-   
-  	case 7:
-   		tmp= retiraFim(genero);
-   		printf("Retirado: %3d\n\n", tmp->num);
-   		system("pause");
-   	break;
-  
-  	case 8:
-   		tmp= retira(genero);
-   		printf("Retirado: %3d\n\n", tmp->num);
-   		system("pause");
-   	break;
-   	
-	case 9:
-   		exibe(genero);
-   		system("pause");
-   	break;
-   	
-   	case 10:
-   		exibe(genero);
-   		system("pause");
-   	break;
-  
-    case 11:
-   		exibe(genero);
-   		system("pause");
-   	break;
-   	
-   	case 12:
-   		exibe(genero);
-   		system("pause");
-   	break;
-   	
-   	case 13:
-   		exibe(genero);
-   		system("pause");
-   	break;
-   	
-   	case 14:
-   		exibe(genero);
-   		system("pause");
-   	break;
-   	
-   	case 15:
-   		libera(genero);
-   		libera(filme);
-   		libera(cliente);
-   		libera(funcionario);
-   		libera(locacao);
-   		system("pause");
-   		return 0;
-   	break;
-   	
-  	default:
-   		printf("Comando inválido\n\n");
-   		system("pause");
-        }    
-    }while (opcao!=15);
-    system("pause"); 
-    return 0;
+    generos *gen = (generos *) malloc(sizeof(generos));
+    filmes *fil = (filmes *) malloc(sizeof(filmes));
+    clientes *clin = (clientes *) malloc(sizeof(clientes));
+    funcionarios *func = (funcionarios *) malloc(sizeof(funcionarios));
+    locacoes *loc = (locacoes *) malloc(sizeof(locacoes));
+    
+    if(!gen && !fil && !clin && !func && !loc){
+        printf("Sem memoria disponivel!\n");
+        exit(1);
+    }
+    else{
+        inicia(gen);
+        inicia_filme(fil);
+        inicia_cliente(clin);
+        inicia_funcionario(func);
+        inicia_locacao(loc);
+        int opt;
+        do{
+        	opt=menu();
+            opcao(gen, fil, clin, func, loc,opt);
+        }while(opt);
+        free(gen);
+        free(fil);
+        return 0;
+    }
 }
